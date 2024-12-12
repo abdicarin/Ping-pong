601677bfe5f1374b46f2631c3adf4fb0f3a19e83
abdicarin <abdicarin.maxamed@elev.ga.ntig.se>
2024-12-12 12:59:32 +0100

    Add files via upload

--- pong.py
+++ pong.py
@@ -5,7 +5,6 @@ screen.setup(800,600)
 screen.bgcolor("black") 
 screen.setup
 screen.title("Ping pong")
-screen.listen()
 
 #pilar figur1
 figur1=Turtle()
@@ -14,23 +13,59 @@ def upup():
     screen.onkey("Down")
 
 figur1.penup()
-figur1.setposition(600,0)
+figur1.setposition(-350,0)
 figur1.color("white")
 figur1.shape("square")
 figur1.color("white")
 figur1.shapesize(4,0.5)
 #pilar figur2
 figur2=Turtle()
-figur2.setposition(-600,0)
+figur2.setposition(350,0)
 figur2.color("white")
 figur2.shape("square")
 figur2.color("white")
 figur2.shapesize(4,0.5)
 
-figur3=Turtle()
+boll=Turtle()
 #bollen
-figur3.color("white")
-figur3.shape("circle")
+boll.color("white")
+boll.shape("circle")
+def flyta_boll():
+    boll_x=boll.xcor()+10
+    boll_y=boll.ycor()+10
+    boll.goto(boll_x,boll_y)
+
+
+
+
+def flyta_upp1():
+    ny_plats=figur1.ycor()+20
+    figur1.goto(figur1.xcor(), ny_plats)
+
+screen.onkey(flyta_upp1, "Up")
+screen.listen()
+
+game = True
+while game == True:
+    flyta_boll()
+
+def flyta_upp1():
+    ny_plats=figur1.ycor()+20
+    figur1.goto(figur1.xcor(), ny_plats)
+
+def flyta_ner1():
+    ny_plats=figur1.ycor()-20
+    figur1.goto(figur1.xcor(), ny_plats)
+
+screen.onkey(flyta_upp1, "Up")
+screen.onkey(flyta_ner1, "Down")
+screen.listen()
+
+game = True
+while game == True:
+    flyta_boll()
+
+  
 
 
 
